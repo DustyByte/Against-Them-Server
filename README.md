7fbf49a3d6300e3fe52030aa8a4760e2bdda7d17
DustyByte <mashnunurrahman@gmail.com>
2026-06-18 22:48:19 +0600

    Second Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Against-Them-Server
+Acts as a bridge between the developer and all players.
---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('1.0.0');
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Method Not Allowed');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
