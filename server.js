3a0f4fa4192681b77344af7bb1821ba4efe1038d
DustyByte <mashnunurrahman@gmail.com>
2026-06-19 16:23:50 +0600

    test commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('2.0.1');
+    res.end('1.0.0');
   } else {
     res.writeHead(405, { 'Content-Type': 'text/plain' });
     res.end('Method Not Allowed');
